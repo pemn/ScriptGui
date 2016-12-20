f6188e782d82eda736d76f9fdfa2614e0c2b1e7d
Paulo Ernesto <pemn@users.noreply.github.com>
2016-12-20 09:41:23 -0200

    Add files via upload

--- x64/Release/wtlscriptgui.bat
+++ x64/Release/wtlscriptgui.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if "%1" equ "" (
-    echo "usage: $0 input*csv selection=a,b,c numeric=123 boolean@ output*csv"
+    echo "usage: %0 input*csv selection=a,b,c numeric_option=123 boolean_option@ grid_option#csv_column:input output*csv"
     goto :EOF
 )
 
